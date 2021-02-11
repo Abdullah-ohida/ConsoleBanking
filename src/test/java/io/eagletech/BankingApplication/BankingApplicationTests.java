70c72c155de8acc1a0e0b967cce548773e5a68b0
chibuzo <daunikman@gmail.com>
2021-02-11 03:52:32 +0100

    Initial commit

---
+++ src/test/java/io/eagletech/BankingApplication/BankingApplicationTests.java
@@ -0,0 +1,17 @@
+package io.eagletech.BankingApplication;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+public class BankingApplicationTests {
+
+    @BeforeEach
+    void startAllTestsWithThis(){
+
+    }
+
+    @AfterEach
+    void doThisAfterEachTest(){
+
+    }
+}
